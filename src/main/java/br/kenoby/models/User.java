49a37f15b5aeb1c92fcb3d552e7fd0ab8a1afdcb
andrelopes1705 <andrelopes1705@gmail.com>
2022-07-16 12:26:27 -0300

    commit

--- src/main/java/br/kenoby/models/User.java
+++ src/main/java/br/kenoby/models/User.java
@@ -1,17 +1,14 @@
 package br.kenoby.models;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
-public class User implements UserDetails, Comparable<User> {
+public class User  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -82,36 +79,7 @@ public class User implements UserDetails, Comparable<User> {
         this.roles = roles;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-       return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public int compareTo(User o) {
-        return this.compareTo(o);
-    }
-
+    
 
 
 }
