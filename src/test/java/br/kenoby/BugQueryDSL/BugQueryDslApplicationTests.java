49132c5ca7dad3499773d0b549c607ad73b4028a
andrelopes1705 <andrelopes1705@gmail.com>
2022-07-16 13:09:48 -0300

    commit

--- src/test/java/br/kenoby/BugQueryDSL/BugQueryDslApplicationTests.java
+++ src/test/java/br/kenoby/BugQueryDSL/BugQueryDslApplicationTests.java
@@ -1,15 +1,39 @@
 package br.kenoby.BugQueryDSL;
 
-//package br.anaccarati.BugQueryDSL;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BugQueryDslApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+import br.kenoby.models.User;
+import br.kenoby.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BugQueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void createUser(@Autowired UserRepository repository) {
+
+        User user = new User();
+        user.setPassword("xaxa");
+        user.setTunnel(null);
+        user.setUsername("kenoby@sky");
+
+        repository.save(user);
+
+    }
+
+    @Test
+    void exampleTest(@Autowired WebTestClient webClient) {
+        webClient
+                .get().uri("/sys-admin/search")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+}
